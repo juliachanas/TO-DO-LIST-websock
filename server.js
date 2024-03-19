2878193191db080c1b8847dff007b8515ee28909
Julia Chanas <juulia.chanas@gmail.com>
2024-03-19 17:15:03 +0100

    Complete to-do-list

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 const express = require('express');
-
+const socket = require('socket.io');
+const cors = require('cors');
 const app = express();
 
-const server = app.listen(process.env.PORT || 8000, () => {
-  console.log('Server is running...');
+app.use(cors());
+
+const tasks = [];
+
+const server = app.listen(8000, () => {
+  console.log('Server running on port: 8000');
 });
 
 app.use((req, res) => {
   res.status(404).send({ message: 'Not found...' });
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  socket.emit('updateData', tasks);
+
+  socket.on('addTask', (task) => {
+    tasks.push(task);
+    socket.broadcast.emit('addTask', task);
+  });
+
+  socket.on('removeTask', (taskId) => {
+    const taskIndex = tasks.findIndex((elem) => elem.id === taskId);
+    tasks.splice(taskIndex, 1);
+    socket.broadcast.emit('removeTask', taskId);
+  });
 });
